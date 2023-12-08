cb8f8d10bad6bf52cd091ea1b57ffa54f08ecabf
DaniPalma2002 <danielppereira2002@gmail.com>
2023-12-08 17:23:17 +0000

    relva

--- TreesAndTents/TreesAndTents.pl
+++ TreesAndTents/TreesAndTents.pl
@@ -24,14 +24,15 @@ nao_tem_objeto(Tabuleiro, Obj, (L, C)) :-
     obtem_objeto(Tabuleiro, (L, C), O),
     not((O == Obj) ; var(Obj)).
 
-% numero_obj_lista(Lista, Objeto, N)
+% numero_obj_lista(Objeto, Lista, N)
 numero_obj_lista(_, [], 0).
 numero_obj_lista(Obj, [P | R], N) :-
     numero_obj_lista(Obj, R, N1),
     ((var(Obj), var(P); Obj == P, not(var(P))) -> 
         N is N1 + 1; 
         N is N1
-    ).
+    ),
+    !.
 
 % troca_elemento(Lista, Indice, Obj, NovaLista)
 troca_elemento(Lista, Indice, Obj, NovaLista) :- 
@@ -50,6 +51,13 @@ print_tabuleiro(Tabuleiro) :-
     writeln('Tabuleiro:'),
     maplist(writeln, Tabuleiro),
     writeln('--------').
+
+% indice_mesmo_valor(L1, L2, Indice)
+indices_mesmo_valor(L1, L2, Indices) :-
+    findall(I, 
+        (nth1(I, L1, Value), nth1(I, L2, Value)), 
+        Indices
+    ).
 
 % Consultas ====================================================================
 
@@ -112,9 +120,31 @@ insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, C1), (L, C2)) :-
     insereObjectoCelula(Tabuleiro, TendaOuRelva, (L, C1)),
     NewC1 is C1 + 1,
     insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, NewC1), (L, C2)).
-
+% AUX: insereObjectoEntrePosicoes_fill(Tabuleiro, IndiceLinhas, TendaOuRelva)
+insereObjectoEntrePosicoes_fill(_, [], _).
+insereObjectoEntrePosicoes_fill(Tabuleiro, [L | R], TendaOuRelva) :-
+    length(Tabuleiro, Len),
+    insereObjectoEntrePosicoes(Tabuleiro, TendaOuRelva, (L, 1), (L, Len)),
+    insereObjectoEntrePosicoes_fill(Tabuleiro, R, TendaOuRelva).
 
 % Estratégias ==================================================================
+
+% relva(Puzzle)
+relva((Tabuleiro, TendasPLinha, TendasPColuna)) :-
+    calculaObjectosTabuleiro(Tabuleiro, CLinhas, CColunas, t), 
+    indices_mesmo_valor(TendasPLinha, CLinhas, IndiceLinhas), 
+    indices_mesmo_valor(TendasPColuna, CColunas, IndiceColunas), 
+    writeln(TendasPLinha), writeln(CLinhas), writeln(IndiceLinhas), nl,
+    writeln(TendasPColuna), writeln(CColunas), writeln(IndiceColunas), nl,
+    insereObjectoEntrePosicoes_fill(Tabuleiro, IndiceLinhas, r),
+    transpose(Tabuleiro, TabTranspose),
+    insereObjectoEntrePosicoes_fill(TabTranspose, IndiceColunas, r),
+    transpose(TabTranspose, Tabuleiro),
+    print_tabuleiro(Tabuleiro),
+    !.
+    
+    
+    
 
 
 
